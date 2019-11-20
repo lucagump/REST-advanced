750c559bb3f10c1641cb25ae51db39ea02b37daa
Constantin <constantin.popa@studenti.unitn.it>
2019-11-20 17:23:10 +0100

    Add Prodct CRUD example

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,3 +4,40 @@ const Product = require('../models/product.model');
 exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
+
+exports.createProduct = function (req, res) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
+
+exports.getProduct = function (req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+exports.updateProduct = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
+        if (err) return next(err);
+        res.send('Product udpated.');
+    });
+};
+
+exports.deleteProduct = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
--- routes.js
+++ routes.js
@@ -6,8 +6,12 @@ const product_controller = require('./controllers/product.controller');
 
 
 // a simple test url to check that all of our files are communicating correctly.
-router.get('/test', product_controller.test);
+router.get('/test', product_controller.test)
+router.post('/products', product_controller.createProduct)
+router.get('/products/:id', product_controller.getProduct)
+router.put('/:id/update', product_controller.updateProduct)
+router.delete('/:id/delete', product_controller.deleteProduct)
 
-router.post('/players', player_controller.createPlayer);
+router.post('/players', player_controller.createPlayer)
 
 module.exports = router;
